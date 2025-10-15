ffe134831906579cea498f54e6d377830d443ff8
Ray <tuhucu97@gmail.com>
2025-10-16 04:25:48 +0900

    feat: .env config  integration

--- apps/carsharing-batch/src/carsharing-batch.module.ts
+++ apps/carsharing-batch/src/carsharing-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarsharingBatchController } from './carsharing-batch.controller';
 import { CarsharingBatchService } from './carsharing-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [CarsharingBatchController],
   providers: [CarsharingBatchService],
 })
